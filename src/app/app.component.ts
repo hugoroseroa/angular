57ddfc56c6cc2b2206e3abc989a283a76d2cc59a
hugoroseroa <hugoroseroa@gmail.com>
2025-07-03 17:24:34 -0500

    Primer proyecto agutar con git

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'todo-list';
+  task="";
+  tasks: {id:number, name:string}[]=[];
+
+  addTask(){
+    if(this.task.trim()==="") return;
+    this.tasks.push({id: this.tasks.length +1, name:this.task});
+  }
+
+  removeTask(id: number){
+    this.tasks=this.tasks.filter(task =>task.id !== id);
+  }
+}
